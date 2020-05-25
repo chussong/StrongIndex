6d7784731df800d9c1375c7f9dd20bd3a8740ef8
chussong <charles.hussong@gmail.com>
2020-05-25 12:09:21 +0900

    Remove inheritance from classes

--- strong-index.hpp
+++ strong-index.hpp
@@ -13,10 +13,14 @@
 
 namespace StrongIndex {
 
-// Every instantiation must have a tag, typically an
-// empty struct like StrongIndex<struct UserIdTag>.
-// T is the underlying type of the index which will 
-// actually be used inside the container.
+// Every instantiation of all index types must have a tag, typically an empty 
+// struct like StrongIndex<struct UserIdTag>. The second parameter, T, is the 
+// underlying type of the index which will actually be used inside the 
+// container.
+
+// A Basic StrongIndex does not allow any direct modification of the value --
+// it has a constructor and assignment from the underlying type, a static cast 
+// into the underlying type, and equality and stream operators.
 template<class Tag, typename T = std::size_t>
 class Basic {
   private:
@@ -26,6 +30,11 @@ class Basic {
   public:
     explicit Basic(T underlyingIndex) noexcept(noThrowIndex):
             index_(underlyingIndex) {
+    }
+
+    Basic& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
+        index_ = underlyingIndex;
+        return *this;
     }
 
     explicit operator T() const noexcept(noThrowIndex) {
@@ -47,24 +56,62 @@ class Basic {
         return os << idx.index_;
     }
 
-  protected:
+  private:
     T index_;
 };
 
+// Incrementable allows ++ and -- operators (pre- and post- are both OK), as
+// well as adding and subtracting by the underlying type. Adding or subtracting
+// from other indices is not allowed.
 template<class Tag, typename T = std::size_t>
-class Incrementable : public Basic<Tag, T> {
+class Incrementable {
+    // Start with the same capabilities as Basic.
+
+  private:
+    static constexpr bool noThrowIndex 
+            = std::is_nothrow_copy_constructible_v<T>;
+
   public:
-    explicit Incrementable(T underlyingIndex) 
-            noexcept(noexcept(Basic<Tag, T>(underlyingIndex))):
-            Basic<Tag, T>(underlyingIndex) {
+    explicit Incrementable(T underlyingIndex) noexcept(noThrowIndex):
+            index_(underlyingIndex) {
     }
 
+    Incrementable& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
+        index_ = underlyingIndex;
+        return *this;
+    }
+
+    explicit operator T() const noexcept(noThrowIndex) {
+        return index_;
+    }
+
+    friend bool operator==(const Incrementable& a, const Incrementable& b) 
+            noexcept(noexcept(a.index_ == b.index_)) {
+        return a.index_ == b.index_;
+    }
+
+    friend bool operator!=(const Incrementable& a, const Incrementable& b)
+            noexcept(noexcept(a == b)) {
+        return !(a == b);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Incrementable& idx) 
+            noexcept(noexcept(os << idx.index_)) {
+        return os << idx.index_;
+    }
+
+  private:
+    T index_;
+
+    // New stuff for Incrementable.
+
+  public:
     Incrementable& operator++() noexcept(noexcept(++this->index_)) {
         ++this->index_;
         return *this;
     }
 
-    Incrementable& operator++(int) noexcept(noexcept(this->index_++)) {
+    Incrementable operator++(int) noexcept(noexcept(this->index_++)) {
         Incrementable oldValue(*this);
         operator++();
         return oldValue;
@@ -75,7 +122,7 @@ class Incrementable : public Basic<Tag, T> {
         return *this;
     }
 
-    Incrementable& operator--(int) noexcept(noexcept(this->index_--)) {
+    Incrementable operator--(int) noexcept(noexcept(this->index_--)) {
         Incrementable oldValue(*this);
         operator--();
         return oldValue;
@@ -92,15 +139,117 @@ class Incrementable : public Basic<Tag, T> {
         this->index_ -= idxShift;
         return *this;
     }
+
+    friend Incrementable operator+(
+            Incrementable a, const T& idxShift)
+            noexcept(noexcept(a += idxShift)) {
+        a += idxShift;
+        return a;
+    }
+
+    friend Incrementable operator-(
+            Incrementable a, const T& idxShift)
+            noexcept(noexcept(a -= idxShift)) {
+        a -= idxShift;
+        return a;
+    }
 };
 
+// FullArithmetic is the most permissive with arithmetic operations.
+// Indices are mostly treated as numbers, and can be added and subtracted
+// from each other, as well as multiplied and divided by the underlying type.
 template<class Tag, typename T = std::size_t>
-class FullArithmetic : public Incrementable<Tag, T> {
+class FullArithmetic {
+    // Start with the same capabilities as Basic.
+
+  private:
+    static constexpr bool noThrowIndex 
+            = std::is_nothrow_copy_constructible_v<T>;
+
   public:
-    explicit FullArithmetic(T underlyingIndex) 
-            noexcept(noexcept(Incrementable<Tag, T>(underlyingIndex))):
-            Incrementable<Tag, T>(underlyingIndex) {
+    explicit FullArithmetic(T underlyingIndex) noexcept(noThrowIndex):
+            index_(underlyingIndex) {
     }
+
+    FullArithmetic& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
+        index_ = underlyingIndex;
+        return *this;
+    }
+
+    explicit operator T() const noexcept(noThrowIndex) {
+        return index_;
+    }
+
+    friend bool operator==(const FullArithmetic& a, const FullArithmetic& b) 
+            noexcept(noexcept(a.index_ == b.index_)) {
+        return a.index_ == b.index_;
+    }
+
+    friend bool operator!=(const FullArithmetic& a, const FullArithmetic& b)
+            noexcept(noexcept(a == b)) {
+        return !(a == b);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const FullArithmetic& idx) 
+            noexcept(noexcept(os << idx.index_)) {
+        return os << idx.index_;
+    }
+
+  private:
+    T index_;
+
+    // Now the full interface from Incrementable.
+
+  public:
+    FullArithmetic& operator++() noexcept(noexcept(++this->index_)) {
+        ++this->index_;
+        return *this;
+    }
+
+    FullArithmetic operator++(int) noexcept(noexcept(this->index_++)) {
+        FullArithmetic oldValue(*this);
+        operator++();
+        return oldValue;
+    }
+
+    FullArithmetic& operator--() noexcept(noexcept(--this->index_)) {
+        --this->index_;
+        return *this;
+    }
+
+    FullArithmetic operator--(int) noexcept(noexcept(this->index_--)) {
+        FullArithmetic oldValue(*this);
+        operator--();
+        return oldValue;
+    }
+
+    FullArithmetic& operator+=(const T& idxShift)
+            noexcept(noexcept(this->index_ += idxShift)) {
+        this->index_ += idxShift;
+        return *this;
+    }
+
+    FullArithmetic& operator-=(const T& idxShift)
+            noexcept(noexcept(this->index_ -= idxShift)) {
+        this->index_ -= idxShift;
+        return *this;
+    }
+
+    friend FullArithmetic operator+(
+            FullArithmetic a, const T& idxShift)
+            noexcept(noexcept(a += idxShift)) {
+        a += idxShift;
+        return a;
+    }
+
+    friend FullArithmetic operator-(
+            FullArithmetic a, const T& idxShift)
+            noexcept(noexcept(a -= idxShift)) {
+        a -= idxShift;
+        return a;
+    }
+
+    // Finally the new stuff for FullArithmetic.
 
     FullArithmetic& operator+=(const FullArithmetic& other)
             noexcept(noexcept(this->index_ += other.index_)) {
@@ -126,26 +275,6 @@ class FullArithmetic : public Incrementable<Tag, T> {
             noexcept(noexcept(a -= b)) {
         a -= b;
         return a;
-    }
-
-    friend FullArithmetic operator+(
-            FullArithmetic a, const T& idxShift)
-            noexcept(noexcept(a += idxShift)) {
-        a += idxShift;
-        return a;
-    }
-
-    friend FullArithmetic operator-(
-            FullArithmetic a, const T& idxShift)
-            noexcept(noexcept(a -= idxShift)) {
-        a -= idxShift;
-        return a;
-    }
-
-    friend FullArithmetic operator+(
-            const T& idxShift, FullArithmetic b)
-            noexcept(noexcept(b + idxShift)) {
-        return b + idxShift;
     }
 
     FullArithmetic& operator*=(const T& idxScale)
