5a59d4b7c671594a6720dfdd22f91b5b27c8ec99
chussong <charles.hussong@gmail.com>
2020-06-10 17:49:45 +0900

    Make all functions constexpr

--- strong-index.hpp
+++ strong-index.hpp
@@ -40,30 +40,30 @@ class Basic {
             = std::is_nothrow_copy_constructible_v<T>;
 
   public:
-    explicit Basic(T underlyingIndex) noexcept(noThrowIndex):
+    constexpr explicit Basic(T underlyingIndex) noexcept(noThrowIndex):
             index_(underlyingIndex) {
     }
 
-    Basic& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
+    constexpr Basic& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
         index_ = underlyingIndex;
         return *this;
     }
 
-    explicit operator T() const noexcept(noThrowIndex) {
+    constexpr explicit operator T() const noexcept(noThrowIndex) {
         return index_;
     }
 
-    friend bool operator==(const Basic& a, const Basic& b) 
+    constexpr friend bool operator==(const Basic& a, const Basic& b) 
             noexcept(noexcept(a.index_ == b.index_)) {
         return a.index_ == b.index_;
     }
 
-    friend bool operator!=(const Basic& a, const Basic& b)
+    constexpr friend bool operator!=(const Basic& a, const Basic& b)
             noexcept(noexcept(a == b)) {
         return !(a == b);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Basic& idx) 
+    constexpr friend std::ostream& operator<<(std::ostream& os, const Basic& idx) 
             noexcept(noexcept(os << idx.index_)) {
         return os << idx.index_;
     }
@@ -84,30 +84,30 @@ class Incrementable {
             = std::is_nothrow_copy_constructible_v<T>;
 
   public:
-    explicit Incrementable(T underlyingIndex) noexcept(noThrowIndex):
+    constexpr explicit Incrementable(T underlyingIndex) noexcept(noThrowIndex):
             index_(underlyingIndex) {
     }
 
-    Incrementable& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
+    constexpr Incrementable& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
         index_ = underlyingIndex;
         return *this;
     }
 
-    explicit operator T() const noexcept(noThrowIndex) {
+    constexpr explicit operator T() const noexcept(noThrowIndex) {
         return index_;
     }
 
-    friend bool operator==(const Incrementable& a, const Incrementable& b) 
+    constexpr friend bool operator==(const Incrementable& a, const Incrementable& b) 
             noexcept(noexcept(a.index_ == b.index_)) {
         return a.index_ == b.index_;
     }
 
-    friend bool operator!=(const Incrementable& a, const Incrementable& b)
+    constexpr friend bool operator!=(const Incrementable& a, const Incrementable& b)
             noexcept(noexcept(a == b)) {
         return !(a == b);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Incrementable& idx) 
+    constexpr friend std::ostream& operator<<(std::ostream& os, const Incrementable& idx) 
             noexcept(noexcept(os << idx.index_)) {
         return os << idx.index_;
     }
@@ -118,48 +118,48 @@ class Incrementable {
     // New stuff for Incrementable.
 
   public:
-    Incrementable& operator++() noexcept(noexcept(++this->index_)) {
+    constexpr Incrementable& operator++() noexcept(noexcept(++this->index_)) {
         ++this->index_;
         return *this;
     }
 
-    Incrementable operator++(int) noexcept(noexcept(this->index_++)) {
+    constexpr Incrementable operator++(int) noexcept(noexcept(this->index_++)) {
         Incrementable oldValue(*this);
         operator++();
         return oldValue;
     }
 
-    Incrementable& operator--() noexcept(noexcept(--this->index_)) {
+    constexpr Incrementable& operator--() noexcept(noexcept(--this->index_)) {
         --this->index_;
         return *this;
     }
 
-    Incrementable operator--(int) noexcept(noexcept(this->index_--)) {
+    constexpr Incrementable operator--(int) noexcept(noexcept(this->index_--)) {
         Incrementable oldValue(*this);
         operator--();
         return oldValue;
     }
 
-    Incrementable& operator+=(const T& idxShift)
+    constexpr Incrementable& operator+=(const T& idxShift)
             noexcept(noexcept(this->index_ += idxShift)) {
         this->index_ += idxShift;
         return *this;
     }
 
-    Incrementable& operator-=(const T& idxShift)
+    constexpr Incrementable& operator-=(const T& idxShift)
             noexcept(noexcept(this->index_ -= idxShift)) {
         this->index_ -= idxShift;
         return *this;
     }
 
-    friend Incrementable operator+(
+    constexpr friend Incrementable operator+(
             Incrementable a, const T& idxShift)
             noexcept(noexcept(a += idxShift)) {
         a += idxShift;
         return a;
     }
 
-    friend Incrementable operator-(
+    constexpr friend Incrementable operator-(
             Incrementable a, const T& idxShift)
             noexcept(noexcept(a -= idxShift)) {
         a -= idxShift;
@@ -179,30 +179,30 @@ class FullArithmetic {
             = std::is_nothrow_copy_constructible_v<T>;
 
   public:
-    explicit FullArithmetic(T underlyingIndex) noexcept(noThrowIndex):
+    constexpr explicit FullArithmetic(T underlyingIndex) noexcept(noThrowIndex):
             index_(underlyingIndex) {
     }
 
-    FullArithmetic& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
+    constexpr FullArithmetic& operator=(const T& underlyingIndex) noexcept(noThrowIndex) {
         index_ = underlyingIndex;
         return *this;
     }
 
-    explicit operator T() const noexcept(noThrowIndex) {
+    constexpr explicit operator T() const noexcept(noThrowIndex) {
         return index_;
     }
 
-    friend bool operator==(const FullArithmetic& a, const FullArithmetic& b) 
+    constexpr friend bool operator==(const FullArithmetic& a, const FullArithmetic& b) 
             noexcept(noexcept(a.index_ == b.index_)) {
         return a.index_ == b.index_;
     }
 
-    friend bool operator!=(const FullArithmetic& a, const FullArithmetic& b)
+    constexpr friend bool operator!=(const FullArithmetic& a, const FullArithmetic& b)
             noexcept(noexcept(a == b)) {
         return !(a == b);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const FullArithmetic& idx) 
+    constexpr friend std::ostream& operator<<(std::ostream& os, const FullArithmetic& idx) 
             noexcept(noexcept(os << idx.index_)) {
         return os << idx.index_;
     }
@@ -213,48 +213,48 @@ class FullArithmetic {
     // Now the full interface from Incrementable.
 
   public:
-    FullArithmetic& operator++() noexcept(noexcept(++this->index_)) {
+    constexpr FullArithmetic& operator++() noexcept(noexcept(++this->index_)) {
         ++this->index_;
         return *this;
     }
 
-    FullArithmetic operator++(int) noexcept(noexcept(this->index_++)) {
+    constexpr FullArithmetic operator++(int) noexcept(noexcept(this->index_++)) {
         FullArithmetic oldValue(*this);
         operator++();
         return oldValue;
     }
 
-    FullArithmetic& operator--() noexcept(noexcept(--this->index_)) {
+    constexpr FullArithmetic& operator--() noexcept(noexcept(--this->index_)) {
         --this->index_;
         return *this;
     }
 
-    FullArithmetic operator--(int) noexcept(noexcept(this->index_--)) {
+    constexpr FullArithmetic operator--(int) noexcept(noexcept(this->index_--)) {
         FullArithmetic oldValue(*this);
         operator--();
         return oldValue;
     }
 
-    FullArithmetic& operator+=(const T& idxShift)
+    constexpr FullArithmetic& operator+=(const T& idxShift)
             noexcept(noexcept(this->index_ += idxShift)) {
         this->index_ += idxShift;
         return *this;
     }
 
-    FullArithmetic& operator-=(const T& idxShift)
+    constexpr FullArithmetic& operator-=(const T& idxShift)
             noexcept(noexcept(this->index_ -= idxShift)) {
         this->index_ -= idxShift;
         return *this;
     }
 
-    friend FullArithmetic operator+(
+    constexpr friend FullArithmetic operator+(
             FullArithmetic a, const T& idxShift)
             noexcept(noexcept(a += idxShift)) {
         a += idxShift;
         return a;
     }
 
-    friend FullArithmetic operator-(
+    constexpr friend FullArithmetic operator-(
             FullArithmetic a, const T& idxShift)
             noexcept(noexcept(a -= idxShift)) {
         a -= idxShift;
@@ -263,71 +263,71 @@ class FullArithmetic {
 
     // Finally the new stuff for FullArithmetic.
 
-    FullArithmetic& operator+=(const FullArithmetic& other)
+    constexpr FullArithmetic& operator+=(const FullArithmetic& other)
             noexcept(noexcept(this->index_ += other.index_)) {
         this->index_ += other.index_;
         return *this;
     }
 
-    FullArithmetic& operator-=(const FullArithmetic& other)
+    constexpr FullArithmetic& operator-=(const FullArithmetic& other)
             noexcept(noexcept(this->index_ -= other.index_)) {
         this->index_ -= other.index_;
         return *this;
     }
 
-    friend FullArithmetic operator+(
+    constexpr friend FullArithmetic operator+(
             FullArithmetic a, const FullArithmetic& b)
             noexcept(noexcept(a += b)) {
         a += b;
         return a;
     }
 
-    friend FullArithmetic operator-(
+    constexpr friend FullArithmetic operator-(
             FullArithmetic a, const FullArithmetic& b)
             noexcept(noexcept(a -= b)) {
         a -= b;
         return a;
     }
 
-    FullArithmetic& operator*=(const T& idxScale)
+    constexpr FullArithmetic& operator*=(const T& idxScale)
             noexcept(noexcept(this->index_ *= idxScale)) {
         this->index_ *= idxScale;
         return *this;
     }
 
-    friend FullArithmetic operator*(
+    constexpr friend FullArithmetic operator*(
             FullArithmetic a, const T& idxScale)
             noexcept(noexcept(a *= idxScale)) {
         a *= idxScale;
         return a;
     }
 
-    friend FullArithmetic operator*(
+    constexpr friend FullArithmetic operator*(
             const T& idxScale, FullArithmetic b)
             noexcept(noexcept(b * idxScale)) {
         return b * idxScale;
     }
 
-    FullArithmetic& operator/=(const T& idxDiv)
+    constexpr FullArithmetic& operator/=(const T& idxDiv)
             noexcept(noexcept(this->index_ /= idxDiv)) {
         this->index_ /= idxDiv;
         return *this;
     }
 
-    friend FullArithmetic operator/(
+    constexpr friend FullArithmetic operator/(
             FullArithmetic a, const T& idxDiv)
             noexcept(noexcept(a /= idxDiv)) {
         a /= idxDiv;
         return a;
     }
 
-    FullArithmetic& operator%=(const T& idxDiv)
+    constexpr FullArithmetic& operator%=(const T& idxDiv)
             noexcept(noexcept(this->index_ %= idxDiv)) {
         this->index_ %= idxDiv;
         return *this;
     }
 
-    friend FullArithmetic operator%(
+    constexpr friend FullArithmetic operator%(
             FullArithmetic a, const T& idxDiv)
             noexcept(noexcept(a %= idxDiv)) {
         a %= idxDiv;
